c20ed3ba945007b9eb9283b70cf3c06906bb5955
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-09-20 17:45:38 +0200

    c file

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 13:04:57 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/12 22:20:05 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/09/20 17:43:23 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 int ft_printf(const char *t, ...)
 {
-    char pr[100];
-    if (ft_check_pourcentage(t, pr) == 0)
-        return (0);
-    va_list parametersInfos;
-    va_start(parametersInfos, t);
-    t++;
-    printf("%s\n", t);
-    return (1);
+    int i;
+
+    i = 0;
+    if(ft_check_pourcentage(t) == -1)
+        return (-1);
+    return (0);
 }
 
 int main(void)
 {
-    printf("%d\n", ft_printf("%c%d%s%p%d%sssi%u%x%X%%", "sss"));
+    printf("\n%d", ft_printf("%d%s%% x%sss", "A"));
 }
