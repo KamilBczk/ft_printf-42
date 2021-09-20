ddc72effee74cd473da3cf900ecae2bb43aeea78
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-09-20 17:45:57 +0200

    h file

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 21:48:00 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/12 22:16:28 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/09/20 17:40:55 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 # define FT_PRINTF_H
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 
 int ft_printf(const char *t, ...);
-int ft_check_pourcentage(const char *s1, char *s);
+int ft_check_pourcentage(const char *s);
 
 #endif
