cc96b5711ced7fd7f26dea58b386bfd425711413
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-09-20 17:45:45 +0200

    c file

--- ft_check_pourcentage.c
+++ ft_check_pourcentage.c
@@ -6,33 +6,56 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 21:46:18 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/12 22:09:31 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/09/20 17:43:42 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_check_pourcentage(const char *s1, char *s)
+void    ft_print_pourcentage(const char *s, char pr, int arg)
+{
+    if (pr == 'c')
+        return ;
+    else if (pr == 's')
+        return ;
+    else if (pr == 'p')
+        return ;
+    else if (pr == 'd')
+        return ;
+    else if (pr == 'i')
+        return ;
+    else if (pr == 'u')
+        return ;
+    else if (pr == 'x')
+        return ;
+    else if (pr == 'X')
+        return ;
+    else if (pr == '%')
+        return ;
+}
+
+int ft_check_pourcentage(const char *s)
 {
     int i;
     int f;
 
     i = 0;
-    f = 0;
-    while (s1[i] != '\0') {
-        if (s1[i] == '%')
+    f = 1;
+    while (s[i] != '\0') {
+        if (s[i] == '%')
         {
-            i++;
-            if (s1[i] == 'c' || s1[i] == 's' || s1[i] == 'p' || s1[i] == 'd' || s1[i] == 'i' || s1[i] == 'u' || s1[i] == 'x' || s1[i] == 'X' || s1[i] == '%')
+            if (s[i + 1] != '\0')
             {
-                s[f] = s1[i];
+                ft_print_pourcentage(s, s[i + 1], f);
                 f++;
+                i++;
             }
             else
-                return (0);
+                return (-1);
         }
+        else
+            write(1, &s[i + 1], 1);
         i++;
     }
-    s[f] = '\0';
-    return (1);
+    return (0);
 }
