a89002047a025b8a437e7d6e18aed1a275cabbbf
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-09-22 22:44:08 +0200

    c file

--- ft_check_pourcentage.c
+++ ft_check_pourcentage.c
@@ -6,56 +6,32 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 21:46:18 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/20 17:43:42 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/09/22 22:36:27 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void    ft_print_pourcentage(const char *s, char pr, int arg)
-{
-    if (pr == 'c')
-        return ;
-    else if (pr == 's')
-        return ;
-    else if (pr == 'p')
-        return ;
-    else if (pr == 'd')
-        return ;
-    else if (pr == 'i')
-        return ;
-    else if (pr == 'u')
-        return ;
-    else if (pr == 'x')
-        return ;
-    else if (pr == 'X')
-        return ;
-    else if (pr == '%')
-        return ;
-}
 
-int ft_check_pourcentage(const char *s)
+int ft_check_percentage(char s)
 {
-    int i;
-    int f;
-
-    i = 0;
-    f = 1;
-    while (s[i] != '\0') {
-        if (s[i] == '%')
-        {
-            if (s[i + 1] != '\0')
-            {
-                ft_print_pourcentage(s, s[i + 1], f);
-                f++;
-                i++;
-            }
-            else
-                return (-1);
-        }
-        else
-            write(1, &s[i + 1], 1);
-        i++;
-    }
+    if (s == 'c')
+        return 0;
+    else if (s == 's')
+        return 0;
+    else if (s == 'p')
+        return 0;
+    else if (s == 'd')
+        return 0;
+    else if (s == 'i')
+        return 0;
+    else if (s == 'u')
+        return 0;
+    else if (s == 'x')
+        return 0;
+    else if (s == 'X')
+        return 0;
+    else if (s == '%')
+        write(1, "%", 1);
     return (0);
 }
