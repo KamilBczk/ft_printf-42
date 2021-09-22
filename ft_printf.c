16c446c3d1135d14e63778f3c178be82caa2d56c
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-09-22 22:44:20 +0200

    c file

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 13:04:57 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/20 17:43:23 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/09/22 22:33:09 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,28 @@ int ft_printf(const char *t, ...)
     int i;
 
     i = 0;
-    if(ft_check_pourcentage(t) == -1)
-        return (-1);
+    while (t[i] != '\0')
+    {
+        if (t[i] == '%')
+        {
+            i++;
+            ft_check_percentage(t[i]);
+        }
+        else
+            write(1, &t[i], 1);
+        i++;
+    }
     return (0);
 }
 
 int main(void)
 {
-    printf("\n%d", ft_printf("%d%s%% x%sss", "A"));
+    ft_printf("Le caractère : \t\t\t|%c|\n");
+    ft_printf("La chaîne de caractère : \t|%s|\n");
+    ft_printf("L'argument de pointeur void *: \t|%p|\n");
+    ft_printf("Nombre en base 10 : \t\t|%d|\n");
+    ft_printf("Entier en base 10 : \t\t|%i|\n");
+    ft_printf("Nombre décimal non signé : \t|%u|\n");
+    ft_printf("Nombre en héxadécimal base 16 : |%x|\n");
+    ft_printf("Pourcentage \t\t\t|%%|");
 }
