2f742b455c3528b85ef7429bc4d9bb4b2647ca5b
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-09-24 21:28:37 +0200

    c file

--- src/ft_printf_numbers.c
+++ src/ft_printf_numbers.c
@@ -6,44 +6,37 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 19:11:09 by kbiczyk           #+#    #+#             */
-/*   Updated: 2021/09/24 17:24:24 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/09/24 20:35:00 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf_u(unsigned int nb, int resetstatic)
+void	ft_printf_u(unsigned int nb, int *finallen)
 {
 	char			z;
-	static int		i;
 
-	if (resetstatic == 0)
-		i = 0;
 	if (nb >= 10)
 	{
-		ft_printf_u(nb / 10, 1);
-		ft_printf_u(nb % 10, 1);
+		ft_printf_u(nb / 10, finallen);
+		ft_printf_u(nb % 10, finallen);
 	}
 	else
 	{
-		i++;
+		*finallen += 1;
 		z = nb + '0';
 		write(1, &z, 1);
 	}
-	return (i);
 }
 
-int	ft_printf_d_i(int nb, int resetstatic)
+void	ft_printf_d_i(int nb, int *finallen)
 {
 	char			z;
 	unsigned int	nbr;
-	static int		i;
 
-	if (resetstatic == 0)
-		i = 0;
 	if (nb < 0)
 	{
-		i++;
+		*finallen += 1;
 		write(1, "-", 1);
 		nbr = (unsigned int)(nb * -1);
 	}
@@ -51,14 +44,13 @@ int	ft_printf_d_i(int nb, int resetstatic)
 		nbr = (unsigned int) nb;
 	if (nbr >= 10)
 	{
-		ft_printf_d_i(nbr / 10, 1);
-		ft_printf_d_i(nbr % 10, 1);
+		ft_printf_d_i(nbr / 10, finallen);
+		ft_printf_d_i(nbr % 10, finallen);
 	}
 	else
 	{
-		i++;
+		*finallen += 1;
 		z = nbr + '0';
 		write(1, &z, 1);
 	}
-	return (i);
 }
