e1bc1713d76e1de00693c1fc077dc91fe4644714
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-09-24 17:26:19 +0200

    h file

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kbiczyk <kbiczyk@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 21:48:00 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/23 19:12:51 by kbiczyk          ###   ########.fr       */
+/*   Updated: 2021/09/24 17:23:46 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@
 
 int		ft_printf(const char *t, ...);
 /*	FT_PRINTF NUMBERS	*/
-void	ft_printf_u(unsigned int nb);
-void	ft_printf_d_i(int nb);
+int	ft_printf_u(unsigned int nb, int resetstatic);
+int	ft_printf_d_i(int nb, int resetstatic);
 /*	FT_PRINTF STRINGS	*/
-void	ft_printf_s(char *str);
-void	ft_printf_c(char c);
+int	ft_printf_s(char *str);
+int	ft_printf_c(char c);
 
 #endif
