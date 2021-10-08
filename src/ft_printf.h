44c9cdab4cc84db95344622f01b40df55114b76c
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-10-08 20:11:11 +0200

    h file

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 21:48:00 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/24 21:23:29 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/10/01 14:27:05 by kbiczyk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int		ft_printf(const char *t, ...);
 void	ft_printf_u(unsigned int nb, int *finallen);
 void	ft_printf_d_i(int nb, int *finallen);
 /*	FT_PRINTF STRINGS	*/
-int	ft_printf_s(char *str);
-int	ft_printf_c(char c);
+int		ft_printf_s(char *str);
+int		ft_printf_c(char c);
 /*  FT_PRINTF HEXADECIMAL NUMBERS    */
 void	ft_printf_p(unsigned long int nb, int *finallen);
-void ft_printf_hexadecimal(unsigned long int nb, int *finallen);
+void	ft_printf_hexadecimal(unsigned long int nb, int *finallen);
 void	ft_printf_x(unsigned int nb, int *finallen, int tohex);
 
 #endif
